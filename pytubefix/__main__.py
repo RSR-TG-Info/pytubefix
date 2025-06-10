6353794acd30c691b79cf2a6e8168bc92805e411
RSR <80620448+RSR-TG-Info@users.noreply.github.com>
2025-06-10 09:59:46 +0530

    Update __main__.py

--- pytubefix/__main__.py
+++ pytubefix/__main__.py
@@ -148,7 +148,7 @@ class YouTube:
 
         # Shared between all instances of `Stream` (Borg pattern).
         self.stream_monostate = Monostate(
-            on_progress=on_progress_callback, on_complete=on_complete_callback
+            on_progress=on_progress_callback, on_complete=on_complete_callback, youtube=self
         )
 
         if proxies:
@@ -347,6 +347,8 @@ class YouTube:
             video = Stream(
                 stream=stream,
                 monostate=self.stream_monostate,
+                po_token=self.po_token,
+                video_playback_ustreamer_config=self.video_playback_ustreamer_config
             )
             self._fmt_streams.append(video)
 
@@ -448,6 +450,29 @@ class YouTube:
                 }
             }
         return self._signature_timestamp
+
+    @property
+    def video_playback_ustreamer_config(self):
+        return self.vid_info[
+            'playerConfig'][
+            'mediaCommonConfig'][
+            'mediaUstreamerRequestConfig'][
+            'videoPlaybackUstreamerConfig']
+
+    @property
+    def server_abr_streaming_url(self):
+        """
+        Extract the url for abr server and decrypt the `n` parameter
+        """
+        try:
+            url = self.vid_info[
+                'streamingData'][
+                'serverAbrStreamingUrl']
+            stream_manifest = [{"url": url}]
+            extract.apply_signature(stream_manifest, vid_info=self.vid_info, js=self.js, url_js=self.js_url)
+            return stream_manifest[0]["url"]
+        except Exception:
+            return None
 
     @property
     def vid_info(self):
